64fb8c98ac095a4785cc6b4e28c0d19a6a89309c
Ryan Johnson <rjohnson@carroll.edu>
2024-01-30 13:16:00 -0700

    Added method comments

--- calculator/models.py
+++ calculator/models.py
@@ -1,6 +1,9 @@
 from django import forms
 
 
+"""
+This class is used to collect and store the necessary data for tip calculations.
+"""
 class TipForm(forms.Form):
     billing_amount = forms.IntegerField(min_value=0, label="Billing Amount")
     tip_amount = forms.IntegerField(min_value=0, max_value=100, label="Tipping Amount (%)")
--- calculator/views.py
+++ calculator/views.py
@@ -4,11 +4,17 @@ from calculator.models import TipForm
 
 
 def index(request):
+    """
+    Displays the home page (index.html), including the form collecting tip data.
+    """
     form = TipForm()
     return render(request, "calculator/index.html", {"form": form})
 
 
 def results(request):
+    """
+    Displays the results page (results.html), stating the calculated tipping dollar amount.
+    """
     form = TipForm(request.POST)
     if form.is_valid():
         tip_amount = '{:,.2f}'.format(form.cleaned_data["billing_amount"] * (form.cleaned_data["tip_amount"] / 100))
